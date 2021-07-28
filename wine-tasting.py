68a24b7c5cb9cd47a0636bf64618297eb8bb57b1
Jayden Lee <34024244+Shroomy1@users.noreply.github.com>
2021-07-28 15:00:20 +1000

    erH

--- wine-tasting.py
+++ wine-tasting.py
@@ -1,4 +1,4 @@
-from math import gamma
+from math import gamma, isnan
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -11,11 +11,14 @@ names = ["fixed acidity","volatile acidity","citric acid","residual sugar","chlo
 df = pd.read_csv(data)
 # print(df.head())
 array = df.values
+fixed_data = "fixed_wine.csv"
+df2 = pd.read_csv(fixed_data)
+fixed_array = df2.values
 print(array[6493])
 print(array.shape)
-array = np.delete(array, np.where(np.isin("nan", array)),axis=1)
-print(array.shape)
-print(array[6493])
+#new_array = np.delete(array, np.where("nan" in array), axis=1)
+print(fixed_array.shape)
+print(fixed_array[6465])
 # X = array[:,1:10]
 # y = array[:,11]
 # X_train, X_validation, Y_train, Y_validation = train_test_split(X, y, test_size=0.20, random_state=1)
